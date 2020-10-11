36a9ba2adf3ecac1a6047a591994e4dc9577b1be
rohit nagappa <rnagappa35@gmail.com>
2020-10-11 15:55:23 +0530

    txn ans sesion on Catogrory Dao

--- src/main/java/com/ecommerce/dao/CategoryDAO.java
+++ src/main/java/com/ecommerce/dao/CategoryDAO.java
@@ -21,10 +21,18 @@ public class CategoryDAO {
 	@Autowired
     private SessionFactory sessionFactory;
 
+	private Session session;
+	private Transaction txn;
+	
 	@SuppressWarnings("unchecked")
 	public Category getCategoryById(long id) {
 		String strId = String.valueOf(id);
-		List<Category> list = this.sessionFactory.getCurrentSession().createQuery("from Category where id=" + strId).list();
+		//List<Category> list = this.sessionFactory.getCurrentSession().createQuery("from Category where id=" + strId).list();
+		session = this.sessionFactory.getCurrentSession();
+		txn = session.beginTransaction();
+		Query query = session.createQuery("from Category where id=" + strId);
+		List<Category> list = query.list();
+		txn.commit();
 		if (list.size() > 0)
 			return (Category) list.get(0);
 		else
@@ -36,17 +44,30 @@ public class CategoryDAO {
 	@SuppressWarnings("unchecked")
 	public void updateCategory(Category category) {
 		String sql = "";
-		if (category.getID() == 0)
-			this.sessionFactory.getCurrentSession().save(category);
-		else if (category.getID() > 0) {
+		if (category.getID() == 0) {
+			//this.sessionFactory.getCurrentSession().save(category);
+			session = this.sessionFactory.getCurrentSession();
+			txn = session.beginTransaction();
+			session.save(category);
+			txn.commit();
+		}else if (category.getID() > 0) {
 			sql = "update Category set name=:name where " +
 					" ID=:id";
-			Query query = this.sessionFactory.getCurrentSession().createQuery(sql);
+			
+			
+			//Query query = this.sessionFactory.getCurrentSession().createQuery(sql);
+			session = this.sessionFactory.getCurrentSession();
+			txn = session.beginTransaction();
+			Query query = session.createQuery(sql);
 			query.setParameter("name", category.getName());
+			
 			if (category.getID() > 0)
 				query.setParameter("id", category.getID());
 			
+			
 			query.executeUpdate();
+			txn.commit();
+			
 		}
 		
 	}
@@ -58,21 +79,32 @@ public class CategoryDAO {
 		String sql = "";
 		sql = "update Product set category_id=0 where category_id=:id";
 
-		Query query = this.sessionFactory.getCurrentSession().createQuery(sql);
+		//Query query = this.sessionFactory.getCurrentSession().createQuery(sql);
+		session = this.sessionFactory.getCurrentSession();
+		txn = session.beginTransaction();
+		Query query = session.createQuery(sql);
 		query.setParameter("id", id);
 		query.executeUpdate();
+		txn.commit();
 
 		sql = "delete from Category where ID=:id";
-		query = this.sessionFactory.getCurrentSession().createQuery(sql);
+		//query = this.sessionFactory.getCurrentSession().createQuery(sql);
+		session = this.sessionFactory.getCurrentSession();
+		txn = session.beginTransaction();
+		query = session.createQuery(sql);
 		query.setParameter("id", id);
 		query.executeUpdate();
-		
+		txn.commit();
 	}
 
 	@SuppressWarnings("unchecked")
 	public List<Category> getAllCategories() {
-		List<Category> list = this.sessionFactory.getCurrentSession().createQuery("from Category order by name").list();
-
+		//List<Category> list = this.sessionFactory.getCurrentSession().createQuery("from Category order by name").list();
+		session = this.sessionFactory.getCurrentSession();
+		txn = session.beginTransaction();
+		Query query = session.createQuery("from Category order by name");
+		List<Category> list = query.list();
+		txn.commit();
 		return list;
 	}
 	
